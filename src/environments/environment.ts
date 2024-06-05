36e69cdc24fcf89ecaa1e0e488c74fd099de4e3a
Sbaa188 <sebad2607@gmail.com>
2024-06-05 11:11:29 -0300

    descargamos el firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// CONFIGURACION DE FIREBASE EN NUESTRA APLICACION WEB 
+export const envarioment = {
+    production: false,
+    //sacamos la palabra "const" y cambiamos el "=" por :,
+     firebaseConfig:{
+        apiKey: "AIzaSyCY22LoipgN6xJvyMxEsyKbYc0eqRX8N18",
+        authDomain: "proyecto-programacion-2024.firebaseapp.com",
+        projectId: "proyecto-programacion-2024",
+        storageBucket: "proyecto-programacion-2024.appspot.com",
+        messagingSenderId: "310652463538",
+        appId: "1:310652463538:web:152c4b0cf23313fe759cab"
+      }
+}
