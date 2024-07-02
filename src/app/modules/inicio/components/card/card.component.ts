dd5110243f7da4ccbdc613cbed1d6d551ef8d3fb
AlejoXDeath <alejoferrada473@gmail.com>
2024-07-02 08:37:56 -0300

    aplicamos media querys en la página: cambio de color y menus activados y desactivados según su tamaño

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -19,7 +19,7 @@ export class CardComponent {
         nombre: "Skyrim", 
         genero: "RPG",
         edad: 2011,
-        imagen: "https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg",                                 
+        imagen: "",                                 
         alt: "Portada de Skyrim"
       },
       {
@@ -27,7 +27,7 @@ export class CardComponent {
         nombre: "Valorant",
         genero: "Shooter",
         edad: 2020,
-        imagen: "https://cdn.vox-cdn.com/thumbor/BndXHLu1pAvYObvUeaRZ9Ik_OFs=/0x0:920x613/1200x800/filters:focal(374x102:520x248)/cdn.vox-cdn.com/uploads/chorus_image/image/66719825/LogoVersion_Beta_Key_Art_VALORANT.0.0.jpg",
+        imagen: "",
         alt: ""
       },
       {
@@ -35,7 +35,7 @@ export class CardComponent {
         nombre: "Hollow Knight",
         genero: "Plataforma",
         edad: 2017,
-        imagen: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70010000003208/4643fb058642335c523910f3a7910575f56372f612f7c0c9a497aaae978d3e51",
+        imagen: "",
         alt: ""
       }
     ]
