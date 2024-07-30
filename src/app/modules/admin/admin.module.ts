a55bf2b32b39b47350ebbb15336d2daa398a11ea
AlejoXDeath <alejoferrada473@gmail.com>
2024-07-30 10:56:40 -0300

    Creamos interfaz de Producto; creamos servicio CRUD; definimos estructura del componente 'Table'; definimos formulario utilizando directivas de Form para producto

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+
+
+// Vista
 import { AdminComponent } from './pages/admin/admin.component';
+
+// Componente
 import { TableComponent } from './components/table/table.component';
+
+// Paqueterías para formularios y formularios reactivos
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Componentes de Angular material
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,11 +24,17 @@ import { TableComponent } from './components/table/table.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   exports:[
     AdminComponent,
-    TableComponent
+    TableComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
